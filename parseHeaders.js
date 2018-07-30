253dfaae4e2de641caf63aa451452e1744826434
David Miller <hawky454@gmail.com>
2018-07-30 15:01:58 -0500

    Finished assignment

--- parseHeaders.js
+++ parseHeaders.js
@@ -1,10 +1,28 @@
-'use strict';
+ 'use strict';
 
-/* eslint-env node */
+ /* eslint-env node */
 
-function parseHeaders(text) {
-    // your code here
-    return text;
-}
+ function parseHeaders(text) {
+     // your code here
+     let obj = {};
+     obj.headers = {};
+     let parsedLines = text.split('\n');
 
-module.exports = parseHeaders;
+     for (let i = 0; i < parsedLines.length; i++) {
+         if (i === 0) {
+             let data = parsedLines[i].split(' ');
+             obj.version = data[0];
+             obj.code = data[1];
+         } else {
+             let newLine = parsedLines[i].split(': ');
+             if (newLine.length === 2) {
+                obj.headers[newLine[0]] = newLine[1];             
+            }
+         }
+         obj.body = parsedLines.join();
+     }
+
+     return obj;
+ }
+
+ module.exports = parseHeaders;
